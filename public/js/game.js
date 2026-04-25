d176a973d26f5269cb59dd191a62eace63baecd1
FlipDeckCards <michael.dano1523@gmail.com>
2026-04-25 14:49:32 -0700

    Update game.js

--- public/js/game.js
+++ public/js/game.js
@@ -114,14 +114,23 @@ function pickZombieType(w) {
 }
 
 function spawnZombie() {
-  const type = pickZombieType(wave);
-  const stats = zombieStats(type, wave);
-  const lane = (Math.random() - 0.5) * LANE_W * LANES;
+  var type = pickZombieType(wave);
+  var stats = zombieStats(type, wave);
+  var side = Math.random();
+  var xPos;
+  if (side < 0.3) {
+    xPos = -LANE_W * 2 - Math.random() * LANE_W * 2;
+  } else if (side > 0.7) {
+    xPos = LANE_W * 2 + Math.random() * LANE_W * 2;
+  } else {
+    xPos = (Math.random() - 0.5) * LANE_W * LANES;
+  }
   zombies.push({
-    x: lane, z: STREET_DEPTH + Math.random() * 40,
-    type, hp: stats.hp, maxHp: stats.hp, speed: stats.speed,
+    x: xPos, z: STREET_DEPTH + Math.random() * 80,
+    type: type, hp: stats.hp, maxHp: stats.hp, speed: stats.speed + (Math.random() * 0.01),
     points: stats.points, attackDmg: stats.attackDmg,
-    flash: 0, wobble: Math.random() * Math.PI * 2, attackCooldown: 0
+    flash: 0, wobble: Math.random() * Math.PI * 2,
+    attackCooldown: 0, sway: (Math.random() - 0.5) * 0.04
   });
 }
 
@@ -313,64 +322,120 @@ function drawGun() {
   var baseY = H + bobY - kickY;
   ctx.save();
   ctx.translate(baseX, baseY);
+
+  // Arms
+  var armGrad = ctx.createLinearGradient(-140, -90, -90, 10);
+  armGrad.addColorStop(0, '#222');
+  armGrad.addColorStop(0.5, '#333');
+  armGrad.addColorStop(1, '#1a1a1a');
+  ctx.fillStyle = armGrad;
+  ctx.beginPath();
+  ctx.moveTo(-130, 10);
+  ctx.quadraticCurveTo(-140, -50, -100, -85);
+  ctx.lineTo(-60, -85);
+  ctx.quadraticCurveTo(-50, -50, -55, 10);
+  ctx.fill();
+  ctx.fillStyle = armGrad;
+  ctx.beginPath();
+  ctx.moveTo(130, 10);
+  ctx.quadraticCurveTo(140, -50, 100, -85);
+  ctx.lineTo(60, -85);
+  ctx.quadraticCurveTo(50, -50, 55, 10);
+  ctx.fill();
+
+  // Gloves
+  ctx.fillStyle = '#111';
+  ctx.beginPath();
+  ctx.moveTo(-95, -90);
+  ctx.lineTo(-55, -90);
+  ctx.lineTo(-45, -70);
+  ctx.lineTo(-105, -70);
+  ctx.fill();
+  ctx.beginPath();
+  ctx.moveTo(95, -90);
+  ctx.lineTo(55, -90);
+  ctx.lineTo(45, -70);
+  ctx.lineTo(105, -70);
+  ctx.fill();
+
+  // Fingers gripping gun
   ctx.fillStyle = '#1a1a1a';
-  ctx.fillRect(-140, -90, 50, 100);
-  ctx.fillStyle = '#2a2a2a';
-  ctx.fillRect(-135, -95, 40, 30);
-  ctx.fillStyle = '#1a1a1a';
-  ctx.fillRect(90, -90, 50, 100);
-  ctx.fillStyle = '#2a2a2a';
-  ctx.fillRect(95, -95, 40, 30);
-  var gunCol = '#3a3a4a';
-  if (curWeapon === 'shotgun') gunCol = '#4a3a2a';
-  if (curWeapon === 'rifle') gunCol = '#2a3a2a';
-  if (curWeapon === 'rocket') gunCol = '#4a2a2a';
-  ctx.fillStyle = gunCol;
+  [-30, -20, -10, 0].forEach(function(x) {
+    ctx.fillRect(x - 5, -105, 8, 20);
+  });
+  [10, 20, 30].forEach(function(x) {
+    ctx.fillRect(x - 3, -105, 8, 20);
+  });
+
+  // Gun body
+  var gGrad = ctx.createLinearGradient(-20, -200, 20, -80);
+  gGrad.addColorStop(0, '#2a2a3a');
+  gGrad.addColorStop(0.3, '#4a4a5a');
+  gGrad.addColorStop(0.7, '#3a3a4a');
+  gGrad.addColorStop(1, '#1a1a2a');
+  ctx.fillStyle = gGrad;
+
   if (curWeapon === 'pistol') {
-    ctx.fillRect(-15, -120, 30, 80);
-    ctx.fillRect(-10, -140, 20, 30);
-    ctx.fillStyle = '#222';
-    ctx.fillRect(-8, -155, 16, 20);
+    ctx.fillRect(-14, -160, 28, 80);
+    ctx.fillRect(-10, -185, 20, 30);
+    ctx.fillRect(-7, -195, 14, 15);
   } else if (curWeapon === 'shotgun') {
-    ctx.fillRect(-12, -130, 24, 90);
-    ctx.fillRect(-8, -180, 16, 60);
-    ctx.fillStyle = '#222';
-    ctx.fillRect(-6, -190, 12, 15);
-    ctx.fillStyle = '#555';
-    ctx.fillRect(-18, -110, 36, 10);
+    ctx.fillRect(-11, -170, 22, 90);
+    ctx.fillRect(-8, -230, 16, 70);
+    ctx.fillRect(-14, -150, 28, 10);
   } else if (curWeapon === 'rifle') {
-    ctx.fillRect(-10, -130, 20, 90);
-    ctx.fillRect(-7, -200, 14, 80);
-    ctx.fillStyle = '#222';
-    ctx.fillRect(-5, -210, 10, 15);
-    ctx.fillStyle = '#555';
-    ctx.fillRect(-20, -120, 40, 8);
-    ctx.fillRect(-3, -220, 6, 15);
-  } else if (curWeapon === 'rocket') {
-    ctx.fillRect(-18, -130, 36, 90);
-    ctx.fillRect(-14, -190, 28, 70);
-    ctx.fillStyle = '#222';
-    ctx.fillRect(-16, -195, 32, 10);
-    ctx.fillStyle = '#555';
-    ctx.fillRect(-22, -100, 44, 10);
+    ctx.fillRect(-9, -170, 18, 90);
+    ctx.fillRect(-6, -250, 12, 90);
+    ctx.fillRect(-16, -155, 32, 8);
+    ctx.fillRect(-4, -265, 8, 20);
+  } else {
+    ctx.fillRect(-16, -170, 32, 90);
+    ctx.fillRect(-12, -230, 24, 70);
+    ctx.fillRect(-20, -140, 40, 12);
   }
-  ctx.fillStyle = 'rgba(255,255,255,0.05)';
-  ctx.fillRect(-12, -120, 8, 60);
+
+  // Neon accent line
+  ctx.shadowColor = w.col;
+  ctx.shadowBlur = 12;
+  ctx.strokeStyle = w.col;
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.moveTo(-12, -150);
+  ctx.lineTo(-12, -100);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(12, -150);
+  ctx.lineTo(12, -100);
+  ctx.stroke();
+  ctx.shadowBlur = 0;
+
+  // Barrel tip glow
+  ctx.fillStyle = w.col;
+  ctx.shadowColor = w.col;
+  ctx.shadowBlur = 8;
+  ctx.beginPath();
+  var tipY = curWeapon === 'rifle' ? -265 : curWeapon === 'shotgun' ? -230 : curWeapon === 'rocket' ? -230 : -195;
+  ctx.arc(0, tipY, 3, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.shadowBlur = 0;
+
   ctx.restore();
+
+  // Muzzle flash
   if (muzzleFlash > 0) {
     var mfX = baseX;
-    var mfY = baseY - 180;
-    var mfSize = 30 + (curWeapon === 'rocket' ? 50 : curWeapon === 'shotgun' ? 40 : 15);
+    var mfY = baseY + tipY + 5;
+    var mfSize = 25 + (curWeapon === 'rocket' ? 55 : curWeapon === 'shotgun' ? 45 : curWeapon === 'rifle' ? 20 : 15);
     ctx.globalAlpha = muzzleFlash;
     ctx.fillStyle = '#fff';
     ctx.shadowColor = w.col;
-    ctx.shadowBlur = 60;
+    ctx.shadowBlur = 80;
     ctx.beginPath();
     ctx.arc(mfX, mfY, mfSize * muzzleFlash, 0, Math.PI * 2);
     ctx.fill();
     ctx.fillStyle = w.col;
     ctx.beginPath();
-    ctx.arc(mfX, mfY, mfSize * muzzleFlash * 0.5, 0, Math.PI * 2);
+    ctx.arc(mfX, mfY, mfSize * muzzleFlash * 0.4, 0, Math.PI * 2);
     ctx.fill();
     ctx.shadowBlur = 0;
     ctx.globalAlpha = 1;
@@ -490,7 +555,7 @@ function update(dt) {
   zombies.forEach(z => {
     z.z -= z.speed*dt*60;
     z.wobble += dt*(z.type==='runner'?12:6);
-    z.x += Math.sin(z.wobble*0.3)*0.02;
+    z.x += Math.sin(z.wobble * 0.3) * 0.05 + z.sway;
     if (z.flash > 0) z.flash -= dt;
 
     if (z.z <= ATTACK_RANGE) {
