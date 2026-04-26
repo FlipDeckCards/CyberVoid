ce7516c95ba984e869007b6779a9e4c81fed9463
FlipDeckCards <michael.dano1523@gmail.com>
2026-04-25 18:51:53 -0700

    Update game.js

--- public/js/game.js
+++ public/js/game.js
@@ -515,9 +515,9 @@ function drawGun() {
   // Per-weapon orientation: rot in radians, flipX mirrors horizontally
   var gunConfig = {
     pistol:  { rot: 0,    flipX: false },
-    shotgun: { rot: .3,    flipX: false },
-    rifle:   { rot: .3,    flipX: false },
-    rocket:  { rot: .5,    flipX: false }
+    shotgun: { rot: 0,    flipX: false },
+    rifle:   { rot: 0,    flipX: false },
+    rocket:  { rot: 0,    flipX: false }
   };
 
   var gunImg;
