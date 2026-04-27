e7fff1d8a13b78d6f974e1f9d8d6a25fb3813537
FlipDeckCards <michael.dano1523@gmail.com>
2026-04-26 19:00:53 -0700

    Update game.js

--- public/js/game.js
+++ public/js/game.js
@@ -752,7 +752,7 @@ function drawHUD() {
   var isMobileHUD = W < 800;
   var invW = isMobileHUD ? 56 : 42;
   var invH = isMobileHUD ? 38 : 26;
-  var invY = isMobileHD ? H - 170 : H - 95;
+  var invY = isMobileHD ? H - 110 : H - 95;
   var invStart = cx - (WEAPON_ORDER.length * invW) / 2;
   weaponBtnRects = [];
 
