0abc1e11cec296e9336fff881666b6f310fe2133
FlipDeckCards <michael.dano1523@gmail.com>
2026-05-02 21:32:17 -0700

    add draco loader

--- public/zombies/assets/map/arena.js
+++ public/zombies/assets/map/arena.js
@@ -1,8 +1,11 @@
 // assets/map/arena.js
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.6/');
 const loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader);
 const cache = {};
 
 console.log("scene from window:", window.scene);
