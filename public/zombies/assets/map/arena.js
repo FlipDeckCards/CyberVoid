df31413e98bda9cfbb6611f9c04bf1aea3049100
FlipDeckCards <michael.dano1523@gmail.com>
2026-05-03 19:20:56 -0700

    restore slim arena.js for new map

---
+++ public/zombies/assets/map/arena.js
@@ -0,0 +1,18 @@
+import * as THREE from 'three';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.6/');
+const loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader);
+
+const scene = window.scene;
+
+loader.load('./assets/map/mega_village.glb', (gltf) => {
+  const mesh = gltf.scene;
+  mesh.scale.setScalar(50);
+  mesh.position.set(0, 0, 0);
+  scene.add(mesh);
+  console.log("map loaded");
+});
