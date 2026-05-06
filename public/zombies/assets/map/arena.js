443d87708ecd904739134e89aaeae4775b01e113
FlipDeckCards <michael.dano1523@gmail.com>
2026-05-06 08:25:58 -0700

    floor at 0.25

--- public/zombies/assets/map/arena.js
+++ public/zombies/assets/map/arena.js
@@ -12,7 +12,7 @@ const scene = window.scene;
 loader.load('./assets/map/MIKE2.0_compressed.glb', (gltf) => {
   const mesh = gltf.scene;
   mesh.scale.setScalar(0.8);
-  mesh.position.set(0, 1.0, 0);
+  mesh.position.set(0, 0.25, 0);
   scene.add(mesh);
   console.log("map loaded");
 });
