a9e92736d0ff6789c231a4da674ec6a11c396dbb
FlipDeckCards <michael.dano1523@gmail.com>
2026-05-06 08:01:43 -0700

    raise floor to 0.5

--- public/zombies/assets/map/arena.js
+++ public/zombies/assets/map/arena.js
@@ -12,7 +12,7 @@ const scene = window.scene;
 loader.load('./assets/map/MIKE2.0_compressed.glb', (gltf) => {
   const mesh = gltf.scene;
   mesh.scale.setScalar(1.2);
-  mesh.position.set(0, -3.75, 0);
+  mesh.position.set(0, 0.5, 0);
   scene.add(mesh);
   console.log("map loaded");
 });
