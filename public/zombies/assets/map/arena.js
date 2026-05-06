0105690dcc7aab4aaaeaeea0308ac5aedbfa3f16
FlipDeckCards <michael.dano1523@gmail.com>
2026-05-06 08:04:06 -0700

    shrink map to 1.0

--- public/zombies/assets/map/arena.js
+++ public/zombies/assets/map/arena.js
@@ -11,7 +11,7 @@ const scene = window.scene;
 
 loader.load('./assets/map/MIKE2.0_compressed.glb', (gltf) => {
   const mesh = gltf.scene;
-  mesh.scale.setScalar(1.2);
+  mesh.scale.setScalar(1.0);
   mesh.position.set(0, 0.5, 0);
   scene.add(mesh);
   console.log("map loaded");
