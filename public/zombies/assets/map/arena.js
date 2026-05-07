eb3f7b7aa0b8a8700a78d3fb3038206ff6341d7a
FlipDeckCards <michael.dano1523@gmail.com>
2026-05-06 23:33:28 -0700

    lower map to -5

--- public/zombies/assets/map/arena.js
+++ public/zombies/assets/map/arena.js
@@ -12,7 +12,7 @@ const scene = window.scene;
 loader.load('./assets/map/MIKE2.0_compressed.glb', (gltf) => {
   const mesh = gltf.scene;
   mesh.scale.setScalar(0.8);
-  mesh.position.set(0, -2, 0);
+  mesh.position.set(0, -5, 0);
   scene.add(mesh);
   console.log("map loaded");
 });
